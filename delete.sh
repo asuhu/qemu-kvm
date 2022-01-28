cc99469032942bc923bc3ba5add746803a46dbba
asuhu <860116511@qq.com>
2022-01-28 15:10:51 +0800

    Update

--- delete.sh
+++ delete.sh
@@ -12,11 +12,11 @@ else
 		 virsh undefine ${name} --snapshots-metadata --remove-all-storage --nvram
 		 echo "Successfully delete ${name}."
 		 echo -e "\e[1;33;41m Successfully delete ${name}. \e[0m"
-		 echo "${deletetime}" "-" "Instance Name ${name}". >>/root/instance.log
+		 echo "${deletetime}" "-" "Instance Name ${name}" >>/root/instance.log
 	else
 		 virsh change-media ${name} hda --eject --config --force 2>/dev/null
 		 virsh change-media ${name} sda --eject --config --force 2>/dev/null
 		 virsh undefine ${name} --snapshots-metadata --remove-all-storage --nvram
-		 echo "${deletetime}" "-" "Instance Name ${name}". >>/root/instance.log
+		 echo "${deletetime}" "-" "Instance Name ${name}" >>/root/instance.log
 	fi
 fi
